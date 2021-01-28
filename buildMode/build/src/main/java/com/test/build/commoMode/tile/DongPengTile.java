a2f0b3fd3d59fd175a118912a741902a2df59d25
jack <123@163.com>
2021-01-28 17:52:48 +0800

    建设者模式第一次提交

---
+++ buildMode/build/src/main/java/com/test/build/commoMode/tile/DongPengTile.java
@@ -0,0 +1,34 @@
+package com.test.build.commoMode.tile;
+
+import java.math.BigDecimal;
+
+import com.test.build.commoMode.Matter;
+
+public class DongPengTile implements Matter {
+
+	@Override
+	public String scene() {
+		return "地砖";
+	}
+
+	@Override
+	public String brand() {
+		return "东鹏瓷砖";
+	}
+
+	@Override
+	public String model() {
+		 return "10001";
+	}
+
+	@Override
+	public BigDecimal price() {
+		return new BigDecimal(102);
+	}
+
+	@Override
+	public String desc() {
+		return "东鹏瓷砖以品质铸就品牌，科技推动品牌，口碑传播品牌为宗旨，2014年品牌价值132.35亿元，位列建陶行业榜首。";
+	}
+
+}
